93d033ff5c249e56973ba52450f008de10e1ecc9
pengeg <noreply@pengeg.com>
2026-02-28 18:52:39 -0500

    fix noctalia exec

--- modules/home/desktop/hyprland.nix
+++ modules/home/desktop/hyprland.nix
@@ -133,11 +133,8 @@
         "wl-paste --type text --watch cliphist store"
         "wl-paste --type image --watch cliphist store"
         "systemctl --user start hyprpolkitagent"
+        "noctalia-shell"
         "keepassxc --minimized"
-      ];
-
-      exec = [
-        "noctalia-qs -c noctalia-shell"
       ];
 
       # Window rules
