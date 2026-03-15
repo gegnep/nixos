e512a038986b27bfe71de35d67dead1907a9a804
pengeg <noreply@pengeg.com>
2026-03-14 23:21:20 -0400

    same as last but there was a $ somewhere

--- modules/home/desktop/xdg.nix
+++ modules/home/desktop/xdg.nix
@@ -23,7 +23,7 @@
       createDirectories = true;
       desktop = "${config.home.homeDirectory}/desktop";
       documents = "${config.home.homeDirectory}/documents";
-      download = "${$config.home.homeDirectory}/downloads";
+      download = "${config.home.homeDirectory}/downloads";
       music = "${config.home.homeDirectory}/music";
       pictures = "${config.home.homeDirectory}/pictures";
       publicShare = null;
