b1609bb2e8ba85a22f6cc8714898ac71804f99a2
pengeg <noreply@pengeg.com>
2026-03-20 09:39:38 -0400

    UPDATE flake

--- flake.nix
+++ flake.nix
@@ -9,44 +9,38 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # Theming
-    catppuccin.url = "github:catppuccin/nix";
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-    # Programs
+    catppuccin.url = "github:catppuccin/nix";
     spicetify-nix.url = "github:Gerg-L/spicetify-nix";
     nix-alien.url = "github:thiagokokada/nix-alien";
 
-    # Desktop
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # Kernel/performance
     cachynix.url = "github:ByteZ1337/CachyNix";
   };
 
   outputs = { nixpkgs, home-manager, ... }@inputs:
   let
-    # Helper to create nixosSystem with common config
     mkHost = { hostname, extraModules ? [] }: nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs; };
       modules = [
         { nixpkgs.hostPlatform = "x86_64-linux"; }
         ./hosts/${hostname}
-
-        # Overlays
         {
           nixpkgs.overlays = [
             inputs.nix-alien.overlays.default
             inputs.cachynix.overlays.default
+            inputs.nur.overlays.default
           ];
         }
-
-        # Third-party NixOS modules
         inputs.catppuccin.nixosModules.catppuccin
-
-        # Home Manager
         home-manager.nixosModules.home-manager
         {
           home-manager = {
