7882c119e319299f40460d71f3eb60ab7b994ad1
pengeg <noreply@pengeg.com>
2026-05-21 11:33:35 -0400

    kiro & slack for work

--- modules/home/programs/laptop.nix
+++ modules/home/programs/laptop.nix
@@ -2,13 +2,16 @@
 
 {
   home.packages = with pkgs; [
+    # general laptop utils
     libwacom
     wvkbd
-
     rnote
     xournalpp
-
     wl-mirror
     hyprpicker
+
+    # work
+    slack
+    kiro
   ];
 }
